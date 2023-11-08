0651d8f8b681ea073c07c12ad9ea919188b6fa02
Jaime Dargallo <jaimedargallo@gmail.com>
2023-11-08 13:25:49 +0100

    add specific sounds

--- index.js
+++ index.js
@@ -5,8 +5,45 @@ for (var i = 0; i<numberOfDrumButtons; i++) {
 
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
       
-        var audio = new Audio("sounds/crash.mp3");
-        audio.play();
+        switch(buttonInnerHTML) {
+            case "w":
+                var tom1 = new Audio("sounds/tom-1.mp3");
+                audio.play();
+            break;
+
+            case "a":
+                var tom2 = new Audio("sounds/tom-2.mp3");
+                audio.play();
+            break;
+
+            case "s":
+                var tom3 = new Audio("sounds/tom-3.mp3");
+                audio.play();
+            break;
+
+            case "d":
+                var tom4 = new Audio("sounds/tom-4.mp3");
+                audio.play();
+            break;
+
+            
+            case "j":
+                var snare = new Audio("sounds/snare.mp3");
+                audio.play();
+            break;
+
+            case "k":
+                var crash = new Audio("sounds/crash.mp3");
+                audio.play();
+            break;
+
+            case "l":
+                var kick = new Audio("sounds/kick-bass.mp3");
+                audio.play();
+            break;
+
+            default: console.log()
+        }
     });
 
 }
